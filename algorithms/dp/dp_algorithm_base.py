a0ef6d49da2d87c9cb6de192b6834417900de1ac
Alex <a.giavaras@gmail.com>
2021-07-09 18:37:40 +0100

    Fix DP base algo API

--- algorithms/dp/dp_algorithm_base.py
+++ algorithms/dp/dp_algorithm_base.py
@@ -1,5 +1,18 @@
+from typing import Any
 from algorithms.algorithm_base import AlgorithmBase
 
+
 class DPAlgoBase(AlgorithmBase):
-    pass
-    
+    """
+    Base class for DP-based algorithms
+    """
+
+    def __init__(self, n_max_iterations: int, tolerance: float,
+                 env: Any, gamma: float) -> None:
+        super(DPAlgoBase, self).__init__(n_max_iterations=n_max_iterations,
+                                         tolerance=tolerance, env=env)
+        self._gamma = gamma
+
+    @property
+    def gamma(self) -> float:
+        return self._gamma
