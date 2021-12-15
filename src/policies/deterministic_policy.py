a8899b06989f2e17c7efdcf56ba7f6e34a5ff9cf
Alex <a.giavaras@gmail.com>
2021-12-15 10:01:39 +0000

    #33 Add deterministic policy

---
+++ src/policies/deterministic_policy.py
@@ -0,0 +1,23 @@
+import numpy as np
+from typing import TypeVar
+
+from src.policies.policy_adaptor_base import PolicyAdaptorBase
+
+PolicyBase = TypeVar('PolicyBase')
+
+
+class DeterministicAdaptorPolicy(PolicyAdaptorBase):
+
+    """
+    Update a policy by choosing the best action
+    """
+
+    def __init__(self) -> None:
+        super(DeterministicAdaptorPolicy, self).__init__()
+
+    def __call__(self, policy: PolicyBase, *args, **kwargs) -> PolicyBase:
+        s: int = kwargs["s"]
+        state_actions: np.ndarray = kwargs["state_actions"]
+        action = np.argmax(state_actions)
+        policy[s][action] = 1.0
+        return policy
