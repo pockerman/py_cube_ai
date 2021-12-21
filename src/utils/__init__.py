d922e815c190c5237294a8ec201602d1d28f4b22
Alex <a.giavaras@gmail.com>
2021-12-21 10:15:21 +0000

    #45 Rename algorithm

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
-INFO = "INFO: "
-WARNING = "WARNING: "
-ERROR = "ERROR: "
+from src.utils.log_utils import LogMsg
+INFO = LogMsg.INFO.name #"INFO: "
+WARNING = LogMsg.WARNING.name #"WARNING: "
+ERROR = LogMsg.ERROR.name #"ERROR: "
