92c36ac3232574f9f70befb0a2cef2e954e31e5d
Alex <a.giavaras@gmail.com>
2022-01-08 18:40:40 +0000

    Update documentation and controller

--- src/apps/webots/diff_drive_sys/controllers/EPuckGoForward/EPuckGoForward.py
+++ src/apps/webots/diff_drive_sys/controllers/EPuckGoForward/EPuckGoForward.py
@@ -7,18 +7,28 @@ https://photo.stackexchange.com/questions/12434/how-do-i-calculate-the-distance-
 The physical data for the model can be found here
 
 https://cyberbotics.com/doc/guide/epuck#e-puck-model
+
+Simulation of a differential drive system. In this simulation, we train a Q-learning
+agent using the distance from the distance sensor. The environment is an empty
+environment surrounded by a wall. The agent is located in the middle of he world
+Its goal is to move around the perimeter of the environment without bumping on the wall.
+Every time the robot bumps on the wall, the episode finishes and the agent receives a reward
+of -1. Every time the agent completes a round it receives  a reward of 1.
+If the agent is trapped in a corner.
+
+The description of the epuck robot can be found at: https://cyberbotics.com/doc/guide/epuck#e-puck-model
 """
 
-import cv2
-from collections import deque
+from collections import namedtuple
+
 # You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor, Camera, DistanceSensor
 from src.apps.webots.diff_drive_sys.controllers.motor_wrapper import init_robot_motors
+from src.apps.webots.diff_drive_sys.controllers.action_space import ActionBase
+from src.apps.webots.diff_drive_sys.controllers.proximity_sensors_wrapper import init_robot_proximity_sensors, read_proximity_sensors
 from src.algorithms.td.q_learning import QLearning
 from src.utils import INFO
-
-MAX_SPEED = 6.28
+from src.worlds.time_step import TimStep
 
 # Define a variable that defines the duration of each physics step.
 # This macro will be used as argument to the Robot::step function,
@@ -30,47 +40,31 @@ TIME_STEP = 64
 SPEED_RATE_FACTOR = 0.1
 WALL_REAL_HEIGHT = 0.1
 
+MAX_SPEED = 6.28
+MIN_SPEED = 0.0
+
+# threshold value for the proximity sensors
+# to identify the fact that the robot crushed the wall
+BUMP_THESHOLD = 3520
+
+State = namedtuple("State", ["Sensors", "Motors"])
+
 # create the Robot instance.
 robot = Robot()
 
-camera = robot.getDevice("camera")
-
-camera.enable(samplingPeriod=5)
-
-camera_width = camera.getWidth()
-camera_height = camera.getHeight()
-
-print("{0} Camera width/height {1}/{2}".format(INFO, camera_width, camera_height))
-byte_size = camera_width * camera_height * 4
-
-# camera focal length
-f = camera.getFocalLength()
-
+# get the motors
 leftMotor, rightMotor = init_robot_motors(robot=robot, left_motor_vel=0.0, right_motor_vel=0.0)
-
-"""
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
-
-# set the target position of the motors
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
-
-# set up the motor speeds at 10% of the MAX_SPEED.
-leftMotor.setVelocity(0.1 * MAX_SPEED)
-rightMotor.setVelocity(0.1 * MAX_SPEED)
-"""
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-print("{0} Time step size {1}".format(INFO, timestep))
+proximity_sensors = init_robot_proximity_sensors(robot=robot, sampling_period=TIME_STEP)
 
 # the two front distance sensors
-ps0 = robot.getDevice(name="ps0")
-ps0.enable(samplingPeriod=TIME_STEP)
+ps0 = proximity_sensors[0]
+ps7 = proximity_sensors[-1]
 
-ps7 = robot.getDevice(name="ps7")
-ps7.enable(samplingPeriod=TIME_STEP)
+left_wheel_encoder = robot.getDevice("left wheel sensor")
+left_wheel_encoder.enable(samplingPeriod=TIME_STEP)
+
+right_wheel_encoder = robot.getDevice("right wheel sensor")
+right_wheel_encoder.enable(samplingPeriod=TIME_STEP)
 
 print("{0} Distance sensor ps0 type {1}".format(INFO, ps0.getType()))
 print("{0} Distance sensor ps1 type {1}".format(INFO, ps7.getType()))
@@ -83,54 +77,91 @@ print("{0} Distance sensor ps1 type {1}".format(INFO, ps7.getType()))
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
 
-    image = camera.getImage()
+def step(action: ActionBase) -> TimStep:
+    """
+    Perform one step in the environment
+    :return:
+    """
 
-    # add an image to to the image buffer
-    #image_buffer.append(image)
+    # execute the action
+    action.act(robot, TIME_STEP)
 
-    # The values returned by the distance
-    # sensors are scaled between 0 and 4096 (piecewise linearly to the distance).
-    # While 4096 means that a big amount of light is measured (an obstacle is close)
-    # and 0 means that no light is measured (no obstacle).
-
+    # check if the robot crushed in the environment
     # detect obstacles
-    right_obstacle = ps0.getValue() > 80.0
-    left_obstacle = ps7.getValue() > 80.0
+    proximity_sensor_vals = read_proximity_sensors(sensors=proximity_sensors, threshold=BUMP_THESHOLD)
 
-    # initialize motor speeds at 50% of MAX_SPEED.
-    leftSpeed = 0.5 * MAX_SPEED
-    rightSpeed = 0.5 * MAX_SPEED
-    # modify speeds according to obstacles
+    # we may have finished because the goal was reached
+    done = proximity_sensor_vals[-1]
 
-    if right_obstacle and left_obstacle:
-        leftSpeed = 0.0
-        rightSpeed = 0.0
-    elif left_obstacle:
-        # turn right
+    reward = 1.0
+
+    if proximity_sensor_vals[-1]:
+        reward = -1.0
+
+    state = State()
+    time_step = TimStep(state=state, reward=reward, done=done, info={})
+
+    left_encoder_pos = left_wheel_encoder.getValue()
+    right_encoder_pos = right_wheel_encoder.getValue()
+
+
+
+    # how do we check if we have crushed a wall?
+
+    # return the distance measures from the wall
+    return time_step
+
+
+
+def main():
+
+    print("Running the controller")
+
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(TIME_STEP) != -1:
+        # Read the sensors:
+        # Enter here functions to read sensor data, like:
+        #  val = ds.getValue()
+
+        print("Position left ", left_wheel_encoder.getValue())
+        print("Position right ", right_wheel_encoder.getValue())
+
+        # The values returned by the distance
+        # sensors are scaled between 0 and 4096 (piecewise linearly to the distance).
+        # While 4096 means that a big amount of light is measured (an obstacle is close)
+        # and 0 means that no light is measured (no obstacle).
+
+        # detect obstacles
+        right_obstacle = ps0.getValue() > 80.0
+        left_obstacle = ps7.getValue() > 80.0
+
+        # initialize motor speeds at 50% of MAX_SPEED.
         leftSpeed = 0.5 * MAX_SPEED
-        rightSpeed = -0.5 * MAX_SPEED
-    elif right_obstacle:
-        # turn left
-        leftSpeed = -0.5 * MAX_SPEED
         rightSpeed = 0.5 * MAX_SPEED
-    # write actuators inputs
-    leftMotor.setVelocity(leftSpeed)
-    rightMotor.setVelocity(rightSpeed)
+        # modify speeds according to obstacles
 
-    print("{0} Distance sensor ps0 type {1}".format(INFO, ps0.getValue()))
-    print("{0} Distance sensor ps1 type {1}".format(INFO, ps7.getValue()))
+        if right_obstacle and left_obstacle:
+            leftSpeed = 0.0
+            rightSpeed = 0.0
+        elif left_obstacle:
+            # turn right
+            leftSpeed = 0.5 * MAX_SPEED
+            rightSpeed = -0.5 * MAX_SPEED
+        elif right_obstacle:
+            # turn left
+            leftSpeed = -0.5 * MAX_SPEED
+            rightSpeed = 0.5 * MAX_SPEED
+        # write actuators inputs
+        leftMotor.setVelocity(leftSpeed)
+        rightMotor.setVelocity(rightSpeed)
 
-    # Process sensor data here.
+        print("{0} Distance sensor ps0 type {1}".format(INFO, ps0.getValue()))
+        print("{0} Distance sensor ps1 type {1}".format(INFO, ps7.getValue()))
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    #pass
 
 # Enter here exit cleanup code.
+if __name__ == '__main__':
+    main()
+
