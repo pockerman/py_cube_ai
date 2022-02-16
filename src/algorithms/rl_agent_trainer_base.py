1799fd134566d0a20a6d3e892771f36c898e83a9
Alex <a.giavaras@gmail.com>
2022-02-16 22:08:58 +0000

    #98 API V1

---
+++ src/algorithms/rl_agent_trainer_base.py
@@ -0,0 +1,75 @@
+"""
+Base class for deriving RL agent trainers
+"""
+import abc
+from typing import TypeVar
+
+Env = TypeVar('Env')
+TrainInfo = TypeVar('TrainInfo')
+Criterion = TypeVar('Criterion')
+PlayInfo = TypeVar('PlayInfo')
+State = TypeVar('State')
+Action = TypeVar('Action')
+Config = TypeVar('Config')
+
+
+class RLAgentTrainerConfig(object):
+    def __init__(self):
+        self.n_episodes: int = 0
+
+
+class RLAgentTrainerBase(metaclass=abc.ABCMeta):
+
+    def __init__(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def get_configuration(self) -> Config:
+        """
+        Returns the configuration of the agent
+        :return:
+        """
+
+    @abc.abstractmethod
+    def train(self, env: Env) -> TrainInfo:
+        """
+
+        :return:
+        :rtype:
+        """
+
+    @abc.abstractmethod
+    def actions_before_training_begins(self, env: Env,  **info) -> None:
+        """
+        Execute any actions the algorithm needs before
+        starting the episode
+        :param env:
+        :param episode_idx:
+        :param info:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def actions_before_episode_begins(self, env: Env,  **info) -> None:
+        """
+        Execute any actions the algorithm needs before
+        starting the episode
+        :param options:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def actions_after_episode_ends(self, env: Env,  **info) -> None:
+        """
+        Execute any actions the algorithm needs after
+        ending the episode
+        :param options:
+        :return:
+        """
+
+    @abc.abstractmethod
+    def actions_after_training_ends(self, env: Env,  **info) -> None:
+        """
+        Execute any actions the algorithm needs after
+        the iterations are finished
+        """
